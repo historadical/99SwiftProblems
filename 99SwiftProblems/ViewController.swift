87dc6788f92f86040b3f1887fc53b00b3ca2e8dd
Nic Laughter <nicholaslaughter@gmail.com>
2018-11-15 10:13:46 -0700

    Initial Commit

---
+++ 99SwiftProblems/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  99SwiftProblems
+//
+//  Created by Nic Laughter on 11/15/18.
+//  Copyright © 2018 Gaire Tech, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
